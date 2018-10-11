59a43833a3504ef86b0d6961662597bf8ee316d8
DoubleAMcCartney <doubleamccartney@gmail.com>
2018-10-11 15:08:24 -0400

    Added move activity

---
+++ app/src/main/java/com/example/android/hangboard/MoveActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.hangboard;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MoveActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+
+}
