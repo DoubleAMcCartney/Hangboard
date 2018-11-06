1f9251b92504c083bbf950b80a670e59c63f8813
DoubleAMcCartney <doubleamccartney@gmail.com>
2018-11-05 23:11:01 -0500

    trying to fix commit

--- app/src/main/java/com/example/android/hangboard/WorkoutListAdapter.java
+++ app/src/main/java/com/example/android/hangboard/WorkoutListAdapter.java
@@ -11,14 +11,26 @@ import android.widget.TextView;
 import java.util.List;
 
 public class WorkoutListAdapter extends
-    RecyclerView.Adapter<WorkoutListAdapter.WorkoutViewHolder> {
+        RecyclerView.Adapter<WorkoutListAdapter.WorkoutViewHolder> {
 
     class WorkoutViewHolder extends RecyclerView.ViewHolder {
         private final TextView workoutItemView;
+        private final TextView repItemView;
+        private final TextView setItemView;
+        private final TextView exerciseItemView;
+        private final TextView workTimeItemView;
+        private final TextView restTimeItemView;
+        private final TextView breakTimeItemView;
 
         private WorkoutViewHolder(View itemView) {
             super(itemView);
             workoutItemView = itemView.findViewById(R.id.textView);
+            repItemView = itemView.findViewById(R.id.reps);
+            setItemView = itemView.findViewById(R.id.sets);
+            exerciseItemView = itemView.findViewById(R.id.exercises);
+            workTimeItemView = itemView.findViewById(R.id.workTime);
+            restTimeItemView = itemView.findViewById(R.id.restTime);
+            breakTimeItemView = itemView.findViewById(R.id.breakTime);
         }
     }
 
@@ -38,6 +50,12 @@ public class WorkoutListAdapter extends
         if (mWorkouts != null) {
             Workout current = mWorkouts.get(position);
             holder.workoutItemView.setText(current.getWorkoutTitle());
+            holder.repItemView.setText(current.getReps());
+            holder.setItemView.setText(current.getSets());
+            holder.exerciseItemView.setText(current.getExercises());
+            holder.workTimeItemView.setText(current.getWorkTime());
+            holder.restTimeItemView.setText(current.getRestTime());
+            holder.breakTimeItemView.setText(current.getBreakTime());
         }
         else {
             holder.workoutItemView.setText("No Workout");
@@ -55,4 +73,6 @@ public class WorkoutListAdapter extends
             return mWorkouts.size();
         else return 0;
     }
+
+
 }
